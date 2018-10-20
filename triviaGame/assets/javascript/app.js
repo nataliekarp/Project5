c3b70dd1567052eb1181187344099db40f52892f
Natalie Karp <karp.natalie@gmail.com>
2018-10-19 22:41:03 -0700

    create the structure for this project

---
+++ triviaGame/assets/javascript/app.js
@@ -0,0 +1,44 @@
+const myQuestions = [
+    {
+        question: "When is Harry Potter's birthday?",
+        answer: {
+           a: "30 July 1980", b: "31 July 1980", c: "31 July 1980", d: "31 July 1981"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What is Harry's middle name?",
+        answer: {
+           a: "Albus", b: "Peter", c: "James", d: "Sirius"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which Muggle school was Harry supposed to attend before getting his Hogwarts letter?",
+        answer: {
+           a: "St Brutus's", b: "Stonewall High", c: "Smeltings", d: "Cokeworth High"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "In which book does Harry find the name for his pet owl, Hedwig?",
+        answer: {
+           a: "The dark forces", b: "Hogwarts: History", c: "Fantastic Beasts", d: "A History of Magic"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "How many birthday cakes did Harry receive when he turned 14?",
+        answer: {
+           a: "3", b: "5", c: "4", d: "2"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which grade is Harry awarded for his History of Magic O.W.L. exam?",
+        answer: {
+           a: "A", b: "D", c: "P", d: "E"
+        },
+        correctAnswer: "b"
+    },
+]
